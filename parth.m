ed66a71d2cd896defc553cd019b411cc23d484b2
ff7058820-sketch <ff7058820@gmail.com>
2026-04-17 09:39:47 +0530

    Add files via upload

---
+++ parth.m
@@ -0,0 +1,13 @@
+% Input two numbers
+a = input('Enter first number: ');
+b = input('Enter second number: ');
+
+% Perform addition
+sum = a + b;
+
+% Perform subtraction
+difference = a - b;
+
+% Display results
+fprintf('Addition: %f\n', sum);
+fprintf('Subtraction: %f\n', difference);
